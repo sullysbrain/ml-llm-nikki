361f71c076b11cf2a06ecfed9febd73100717bff
Scott Sullivan <scott@sullysbrain.com>
2024-04-18 21:56:22 -0400

    Simplified main .py file

--- nikki.py
+++ nikki.py
@@ -1,7 +1,9 @@
 from langchain.chains import LLMChain, ConversationalRetrievalChain
 from langchain.prompts import PromptTemplate
-from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
 from langchain_community.llms import Ollama
+from langchain_community.chat_models import ChatOllama
+
 from langchain_core.output_parsers import StrOutputParser
 
 from langchain.callbacks.manager import CallbackManager
@@ -9,29 +11,27 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain_community.document_loaders.markdown import UnstructuredMarkdownLoader
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 ## HISTORY MEMORY
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferMemory
 #from langchain.callbacks import get_openai_callback
-from langchain_community.callbacks import get_openai_callback
+#from langchain_community.callbacks import get_openai_callback
 import textwrap
+import csv
 
 import nikki_templates
 
 
-# OpenAI specific imports
-# from langchain.agents import load_tools, initialize_agent, AgentType
-# from langchain_openai import OpenAI
-# import os
-# import sys
-# sys.path.append('_private/')
-# from _private import api
-# os.environ['OPENAI_API_KEY'] = api.API_KEY
-
-
-
+# Read CSV file content
+# csv_texts = []
+# with open('_private/logs/SS_Estop_log.csv', newline='', encoding='utf-8') as csvfile:
+#     reader = csv.reader(csvfile)
+#     for row in reader:
+#         # Assuming each row is a list of columns and you concatenate them into one string per row
+#         csv_texts.append(' '.join(row))
 
 loader = UnstructuredMarkdownLoader("_private/reports_all.md")
 docs = loader.load()
@@ -40,52 +40,24 @@ texts = text_splitter.split_text(docs[0].page_content)
 # Convert texts list into a single string of all items
 texts_singletext = ' '.join(texts)
 
+
 # General
 user_question_template = ChatPromptTemplate.from_messages(
     [
-        #('system', 'Here is some context for the conversation: {user_context}'),
-        ("system", " You will obtain information from the prompts as your primary source of data: {user_context} to answer the user's input:"),
-        ("user", "{user_input}")
+        HumanMessagePromptTemplate.from_template("{user_input}")
     ]
 )
-
-
-# OpenAI
-# system_message_template = SystemMessagePromptTemplate.from_template(
-#     "You are a helpful AI bot. Here is context for our conversation {user_context}."
-# )
-# # Format the system message
-# system_message = system_message_template.format(user_context=texts_singletext)
-# chat_template = ChatPromptTemplate.from_messages([
-#     system_message_template,
-#     # Add other messages here...
-# ])
-# # Format all the messages in the template
-# messages = chat_template.format_messages(user_context=texts_singletext, user_input="How are the batteries?")
-
-
-
-complete_context = nikki_templates.nikki_template + user_question_template
 
 
 def chat_with_nikki():
     print("Chat with NIKKI (type 'quit' to end the conversation):")
 
-    # Ollama API
-    # nikki = Ollama(model="llama2:13b", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
-    # nikki = Ollama(model="mixtral:8x22b", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
-    # nikki = Ollama(model="mixtral:8x7b", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
-    nikki = Ollama(model="qwen:32b", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
+    # Ollama API  | options: "llama2:13b", "llama3:8b", "mixtral:8x7b", "qwen:32b"
+    llm = Ollama(model="llama3:8b", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
+    prompt = nikki_templates.nikki_template + user_question_template
+    chain = prompt | llm | StrOutputParser()
 
-    # conversation = ConversationChain(llm=nikki)
-    # conversation_buf = ConversationChain(
-    #     llm=nikki,
-    #     memory=ConversationBufferMemory()
-    # )
-
-    # OpenAI API
-    # llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
-    # nikki = LLMChain(prompt=complete_context, llm=llm)    
+    print("LLM Loaded\n")
 
     while True:
         user_input = input("\n\nYou: ")
@@ -93,13 +65,10 @@ def chat_with_nikki():
             break
 
         # Ollama API
-        formatted_chat = complete_context.format_messages(user_context=texts_singletext, user_input=user_input)
-        nikki.invoke(formatted_chat)
-        #print(conversation.prompt.template)
+        #user_context=reports_split
+        formatted_chat = prompt.format(user_input=user_input)  
 
-        # OpenAI API        
-        # qa = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, return_source_documents=True, combine_docs_chain_kwargs={"prompt": chat_template})
-        # print(nikki.invoke(qa))
+        chain.invoke(formatted_chat)
         
 
 # Example usage
