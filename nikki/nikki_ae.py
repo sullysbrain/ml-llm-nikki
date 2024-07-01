04090bcff83215d9cda4bebf27a035ac179943d8
Scott Sullivan <scott@sullysbrain.com>
2024-07-01 06:57:53 -0400

    Cleaned up file to solid working condition.

--- nikki/nikki_ae.py
+++ nikki/nikki_ae.py
@@ -94,62 +94,75 @@ retriever  = vectordb.as_retriever(k=10)
 
 
 # Contextualize question
-contextualize_q_system_prompt = (
-    "Given a chat history and the latest user question "
-    "which might reference context in the chat history, "
-    "formulate a standalone question which can be understood "
-    "without the chat history. Do NOT answer the question, just "
-    "reformulate it if needed and otherwise return it as is."
-)
-contextualize_q_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", contextualize_q_system_prompt),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
-    ]
-)
-history_aware_retriever = create_history_aware_retriever(
-    llm, retriever, contextualize_q_prompt
-)
+# contextualize_q_system_prompt = (
+#     "Given a chat history and the latest user question "
+#     "which might reference context in the chat history, "
+#     "formulate a standalone question which can be understood "
+#     "without the chat history. Do NOT answer the question, just "
+#     "reformulate it if needed and otherwise return it as is."
+# )
+# contextualize_q_prompt = ChatPromptTemplate.from_messages(
+#     [
+#         ("system", contextualize_q_system_prompt),
+#         MessagesPlaceholder("chat_history"),
+#         ("human", "{input}"),
+#     ]
+# )
+# history_aware_retriever = create_history_aware_retriever(
+#     llm, retriever, contextualize_q_prompt
+# )
 
-# Answer question
-qa_system_prompt = (
-    "You are an assistant for question-answering tasks. Use "
-    "the following pieces of retrieved context to answer the "
-    "question. If you don't know the answer, just say that you "
-    "don't know. Use three sentences maximum and keep the answer "
-    "concise."
-    "
-)
+# # Answer question
+# qa_system_prompt = (
+#     """
+#     You are an assistant for question-answering tasks. Use 
+#     the following pieces of retrieved context to answer the 
+#     question. If you don't know the answer, just say that you 
+#     don't know. Use three sentences maximum and keep the answer 
+#     concise.
 
+#     {context}
+#     """
+# ) 
+# qa_prompt = ChatPromptTemplate.from_messages(
+#     [(“system”, qa_system_prompt), MessagesPlaceholder(“chat_history”), (“human”, “{input}”),
+#                                                                      ]
 
+#         ) # Below we use create_stuff_documents_chain to feed all retrieved context # into the LLM. Note that we can also use StuffDocumentsChain and other # instances of BaseCombineDocumentsChain. question_answer_chain = create_stuff_documents_chain(llm, qa_prompt) rag_chain = create_retrieval_chain(
 
+#             history_aware_retriever, question_answer_chain
 
-# def get_response(user_question, chat_history):
+#         )
 
-#     embedding_function = SentenceTransformerEmbeddings(model_name=EMBED_MODEL)
-#     vectordb = Chroma(
-#         persist_directory=REPORTS_CHROMA_PATH,
-#         embedding_function=embedding_function
-#     )
-#     retriever  = vectordb.as_retriever(k=10)
-    # history = "Your favorite color is blue."
-    # input_param = {"history": history, "context": retriever, "user_question": RunnablePassthrough()}
-
-    # chain = (
-    #     {
-    #         "history": chat_history, 
-    #         "context": retriever, 
-    #         "user_question": RunnablePassthrough()
-    #     }
-    #     | ae_prompt_template
-    #     | llm
-    #     | StrOutputParser()
-    # )
-    # return chain.stream(user_question)
+#         # Usage: chat_history = [] # Collect chat history here (a sequence of messages) rag_chain.invoke({“input”: query, “chat_history”: chat_history})
 
 
-# chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever)
+
+def get_response(user_question, chat_history):
+
+    embedding_function = SentenceTransformerEmbeddings(model_name=EMBED_MODEL)
+    vectordb = Chroma(
+        persist_directory=REPORTS_CHROMA_PATH,
+        embedding_function=embedding_function
+    )
+    retriever  = vectordb.as_retriever(k=10)
+    history = "Your favorite color is blue."
+    input_param = {"history": history, "context": retriever, "user_question": RunnablePassthrough()}
+
+    chain = (
+        {
+            "history": chat_history, 
+            "context": retriever, 
+            "user_question": RunnablePassthrough()
+        }
+        | ae_prompt_template
+        | llm
+        | StrOutputParser()
+    )
+    return chain.stream(user_question)
+
+
+chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever)
 
 
 
